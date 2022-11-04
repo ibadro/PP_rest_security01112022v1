e0e38ddc8f2a213180eab0d8ae8a8e9246ff8fd9
ilnurb <ilnurb@mail.ru>
2022-11-04 15:09:25 +0500

    -JPA

--- src/main/java/pp_rest_security/repository/RoleRepository.java
+++ src/main/java/pp_rest_security/repository/RoleRepository.java
@@ -1,14 +1,13 @@
 package pp_rest_security.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import pp_rest_security.model.Role;
 
 import java.util.List;
 
-
-
 public interface RoleRepository {
     List<Role> getListRoles();
+
     void addRole(Role role);
+
     Role getRoleByName(String role);
 }
