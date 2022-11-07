ee380f2b0bebef289f817bde091def67317998cb
ilnurb <ilnurb@mail.ru>
2022-11-07 13:34:15 +0500

    -JPA

--- src/main/resources/static/appAdmin.js
+++ src/main/resources/static/appAdmin.js
@@ -185,7 +185,7 @@ function deleteUser(){
     const deleteForm = document.forms["formDeleteUser"];
     deleteForm.addEventListener("submit", ev => {
         ev.preventDefault();
-        fetch("http://localhost:8080/api/admin/" + deleteForm.id.value, {
+        fetch("http://localhost:8080/api/users/" + deleteForm.id.value, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
